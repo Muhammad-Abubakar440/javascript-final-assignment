3276234935419282af3bf33232b7277bce2850ba
Muhammad-Abubakar440 <abubakar20525@gmail.com>
2021-04-21 16:01:26 +0500

    program from 15 to30

--- main.js
+++ main.js
@@ -242,3 +242,189 @@
 // if(i%2==0)
 // sum += i;
 // console.log("",sum); 
+
+
+
+// 15. Write a js program to find sum of all odd numbers between 1 to n.
+// var sum=0;
+// const number = parseInt(prompt('Enter a number: '));
+// for(var i=1;i<=number;i++)
+// if(i%2!=0)
+// sum += i;
+// console.log("",sum); 
+
+// 16.Write a js program to print multiplication table of any number
+// var tab ,c;
+// tab=prompt("Enter number for table");
+// for(c=1;c<=10;c++)
+// console.log("",tab,"*",c,"=",tab*c);
+
+// 17.Write a js program to count number of digits in a number.
+
+// function digits_count(n) {
+//     var count = 0;
+//     if (n >= 1) ++count;
+  
+//     while (n / 10 >= 1) {
+//       n /= 10;
+//       ++count;
+//     }
+  
+//     return count;
+//   }
+  
+//   console.log(digits_count(12112));
+  
+//   console.log(digits_count(457));
+
+// 18.  where r is the radius and h is the height of the cylinder.
+
+// 19.Write a js program to find maximum between two numbers.
+// var max;
+// var a=prompt("Enter first number");
+// var b=prompt("Enter second number");
+// max=a;
+// if (b>max) {
+//     max=b;
+//     console.log("The maximum number is",max);
+// }
+
+// 20.Write a js program to find maximum between three numbers.
+
+// var max;
+// var a=prompt("Enter first number");
+// var b=prompt("Enter second number");
+// var c=prompt("Enter third number");
+// max=a;
+// if (b>max) {
+//     max=b;
+
+// if (c>max) 
+//     max=c;
+//     console.log("The maximum number is",max);
+// }
+
+// 21.Write a js program to check whether a number is negative, positive or zero.
+
+// var n=prompt("Enter a number");
+// if (n>0) {
+//     console.log("the number is postive");
+// } else if(n<0)
+//     console.log("The number is negtive");
+
+// else{
+// console.log("The number is zero");
+// }
+//22. Write a js program to check whether a number is divisible by 5 and 11 or not.
+// var n=prompt("Enter a number");
+// if (n%5==0) {
+//     console.log("the number is divisible by 5 ");
+// } else if(n%11==0)
+//     console.log("The number is divisible by 11");
+
+// else{
+// console.log("The number is not divisible by 5 and 11");
+// }
+
+// 23.Write a js program to check whether a number is even or odd.
+
+// var n=prompt("Enter an number");
+// if (n%2==0) {
+//     console.log(n,"is even");
+// } else {
+//     console.log(n,"is odd");
+// }
+// 24. Write a js program to check whether a year is leap year or not.
+
+// var y=prompt("Enter a year");
+// if (y%4==0) {
+//     console.log(y,"is a leap year");
+// } else {
+//     console.log(y,"is not a leap year");
+// }
+
+//25. Write a js program to check whether a character is alphabet or not.
+
+// var ch=prompt("Enter any charater")
+// if (ch>='A'&&ch<='Z') {
+//     console.log("The character",ch,"is a Capital letter.");
+// } else if (ch>='a'&&ch<='z') {
+//     console.log("The character",ch,"is a Small cass letter.");
+// } else {
+//     console.log("The character",ch,"is not alphabet."); 
+// } 
+
+// 26.Write a js program to input any alphabet and check whether it is vowel or consonant.
+
+// var ch=prompt("Enter any character:")
+// if (ch=='A'||ch=='a'||ch=='E'||ch=='e'||ch=='I'||ch=='i'||ch=='O'||ch=='o'||ch=='U'||ch=='u') {
+//     console.log("You enter a Vowel ",ch);
+// } else {
+//     console.log("You enter a consonent");
+// }
+// 27.Write a js program to input any character and check whether it is alphabet, digit or special
+
+// var ch=prompt("Enter any charater")
+// if (ch>='A'&&ch<='Z') {
+//     console.log("The character",ch,"is a Capital letter.");
+// } else if (ch>='a'&&ch<='z') {
+//     console.log("The character",ch,"is a Small cass letter.");
+// } else if (ch>='0'&&ch<='9'){
+//     console.log("The character",ch,"is a digit."); 
+// } else {
+//     console.log("The character",ch,"is a Symbol."); 
+// } 
+
+//28. Write a js program to find cube of any number using function.
+// function cube(n){
+//     var res;
+//     res=n*n*n;
+//     return res;
+// }
+// var get_num;
+// get_num=prompt("Enter a number to find")
+// console.log(cube(get_num));
+
+//29. Write a js program to find diameter 2r, circumference 2pir and area pir*2 of circle using functions.
+
+// function diameter(){
+// var PI=3.14;
+//     var radius=prompt("Enter radius for diameter of circle")
+//     var diameter=2*radius;
+//     return diameter;
+
+// }
+
+// console.log("Diameter of circle",diameter());
+
+// function circumference(){
+//     var PI=3.14;
+//     var radius=prompt("Enter radius for diameter of circle")
+//     var circumference=2*PI*radius;
+//     return circumference;
+// }
+// console.log("Circumference of circle",circumference());
+
+// function area(){
+//     var PI=3.14;
+//     var radius=prompt("Enter radius for diameter of circle")
+
+//     var area=PI*radius*radius;
+//     return area;
+
+// }
+// console.log("Area of circle",area());
+
+//30. Write a js program to find maximum and minimum between two numbers using functions.
+
+// function max(a,b){
+//     var max;
+//        max=a;
+//     if (b>max) {
+//         max=b;
+//     return max;
+//     }
+// }
+// var a=prompt("Enter first number");
+// var b=prompt("Enter second number");
+// console.log("maximum number is:",max(a,b));
